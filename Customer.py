3892bc32afb28bd552fd95ef9c1e0974e1e96f45
user <mutukukennedy34@gmail.com>
2023-08-27 21:27:25 +0300

    codechallenge

--- Customer.py
+++ Customer.py
@@ -2,6 +2,7 @@ class Customer:
     def __init__(self , givenname , familyname):
        self.givenname =givenname
        self.familyname = familyname
+       self._reviews = []
 
     def givenname(self):
         return self.givenname
@@ -17,7 +18,21 @@ class Customer:
 
     def fullnames(self):
         return f"{self.givenname}{self.familyname}"
-    
+
+
+    def restaurants(self):
+        unique_restaurants = set()
+        for review in self._reviews:
+            unique_restaurants.add(review.restaurant().name())
+        return list(unique_restaurants)
+
+    def add_review(self, restaurant, rating):
+        review = Review(self, restaurant, rating)
+        self._reviews.append(review)
+        restaurant.add_review(review)
+
+
+
     def all(self):
         return{
             'givenname' : self.givenname ,
--- Restaurant.py
+++ Restaurant.py
@@ -1,6 +1,22 @@
+from review import Review
+
 class Restaurant:
     def __init__(self ,name):
         self._name = name
+        self._reviews =[]
 
     def name (self):
         return self._name
+    
+    def addreviews(self , reviews):
+        self._reviews.append(reviews)
+
+    def reviews(self):
+        return self._reviews
+    
+    def customers(self):
+        unique_customers = set()
+        for review in self._reviews:
+         unique_customers.add(review.customer().full_name())
+        return list(unique_customers)
+    
